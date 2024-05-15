0b505bdd28879cc19acb5b0173800f65940676d5
angeblow <angeloe@nait.ca>
2024-05-14 21:50:28 -0600

    Started math after git setup

---
+++ workbook/Week2ExampleConsole/Week2Example.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// First off, we need variables!
+// Now variables are DATA! We gotta name them to use them!
+
+// Making a variable requires a TYPE, then NAME, then "=", then VALUE
+string myFavouritePokemonOfAllTime = "Bulbasaur";
+
+int aWholeNumberILike = 4096;
+float aDecimalNumberILike = 12.4f;
+double aMorePreciseDecimalNumber = 0.00000001;
+
+Console.WriteLine(myFavouritePokemonOfAllTime);
+
+Console.WriteLine(aWholeNumberILike + aWholeNumberILike / aDecimalNumberILike * aMorePreciseDecimalNumber);
