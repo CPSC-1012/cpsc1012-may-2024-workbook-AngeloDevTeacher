69e033737d69ebac7a2dda8b0262d0acb9638299
angeblow <angeloe@nait.ca>
2024-05-16 19:45:46 -0600

    Made an intro to datatypes walkthrough

---
+++ workbook/IntroToDataTypes/DataTypeExamples.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// MATH DATA TYPES
+// int, float, double
+
+// An int is an integer - a whole number.
+int aPositiveNumber = 5;
+int aNegativeNumber = -5;
+int zeroIsAValidInt = 0;
+
+// A float is a decimal number - a "floating point" number.
+// If the number is NOT zero, we must add an "f" to the end of the number, as it's different from other decimal numbers.
+
+float aPositiveFloat = 5.0f;
+float aNegativeFloat = -10.5f;
+float zeroIsAValidFloat = 0;
+
+// A double is LIKE a float - it's also a decimal number.
+// HOWEVER, a double takes up TWICE the memory in the program and is more precise!
+// Doubles do NOT need the "f" at the end.
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+
+double aPositiveDouble = 5.0;
+double aNegativeDouble = -99999999.999999;
+double zeroIsAValidDouble = 0;
+
+
+// CHARACTER AND WORD TYPES
+
+// char is for character - a single letter in ''
+char myFirstLetter = 'A';
+
+// string is any collection of characters in a set of ""
+string aEmptyString = "";
+string aBlankString = " ";
+string aNumberButAsAString = "4";
+string theInstructorsName = "Angelo Encarnacion";
+
+// Logical Datatype - the bool
+// bool, short for boolean, can either be true or false;
+bool isItRaining = false;
+bool isCodingCool = true;
