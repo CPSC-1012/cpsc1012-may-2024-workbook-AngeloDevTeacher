5c48fc9256a243310f1753abcc27fb4b17b91971
AngeloDevAB <angeloe@nait.ca>
2024-06-11 20:09:47 -0600

    do while

--- workbook/IntroToLoopsOne/Program.cs
+++ workbook/IntroToLoopsOne/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 // Loops are used to repeat code!
 
 // A while loop will happen WHILE the condition is true.
-while (true)
+// This loop will not run.
+while (false)
 {
     Console.WriteLine("Hello World");
 }
+
+
+// FROM NOW ON: Menus are generally in a do-while with a switch
+do
+{
+    Console.WriteLine("Hello world!");
+} while (false);
