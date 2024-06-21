ae1d2d7aa75a2210fc02dcbdccc6b21383fcebda
AngeloDevAB <angeloe@nait.ca>
2024-06-20 19:10:30 -0600

    Started Array Practice

---
+++ workbook/ArrayPractice/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// This is an exmaple of a list that was declared and initalizxed with values.
+string[] rainbowColours = { "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" };
+
