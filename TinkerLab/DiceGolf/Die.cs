e3406b5aa48db52d087841d5632213e4e2e9c4b0
AngeloDevAB <angeloe@nait.ca>
2024-06-20 21:42:07 -0600

    Rough start to Dice Golf

---
+++ TinkerLab/DiceGolf/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGolf
+{
+    internal class Die
+    {
+        public Die() { }
+        public int DieValue
+        {
+            get {  return dieValue; }
+        }
+        int dieValue = 1;
+        public bool CanRoll = true;
+
+        public void RollDie(Random r)
+        {
+            dieValue = r.Next(1,7);
+        }
+
+
+    }
+}
---
+++ TinkerLab/DiceGolf/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DiceGolf;
+
+Random randomInstance = new Random(0);
+
+const int DICE_COUNT = 5;
+List<Die> diceList = new List<Die>();
+
+string userInput = "";
+bool playAgain = true;
+
+for (int i = 0; i < DICE_COUNT; i++)
+{
+    diceList.Add(new Die());
+}
+
+void AttemptToRollDice()
+{
+    foreach (var dice in diceList)
+    {
+        if (dice.CanRoll)
+        {
+            dice.RollDie(randomInstance);
+        }
+    }
+}
+
+void DisplayDice() {
+    Console.WriteLine($"[{diceList[0].DieValue}], [{diceList[1].DieValue}], [{diceList[2].DieValue}], [{diceList[3].DieValue}], [{diceList[4].DieValue}]");
+}
+
+
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("=====Dice Golf=====\n\n");
+    AttemptToRollDice();
+    DisplayDice();
+    Console.Write("Do you want to roll again?");
+    userInput = Console.ReadLine();
+
+    if (userInput.ToLower()[0] != 'y')
+    {
+        playAgain = false;
+    } 
+} while (playAgain);
+
