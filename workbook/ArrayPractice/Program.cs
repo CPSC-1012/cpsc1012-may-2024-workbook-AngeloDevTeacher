7a8ad145baeccbcb3156dadb7dfe58a55b53fccf
AngeloDevAB <angeloe@nait.ca>
2024-06-20 19:48:49 -0600

    Pig Latin Generator

--- workbook/ArrayPractice/Program.cs
+++ workbook/ArrayPractice/Program.cs
@@ -8,6 +8,17 @@ exampleStringArray[0] = "Hello";
 // Get green from the array.
 Console.WriteLine(rainbowColours[3]);
 
+// Do a method for the data type of the value of the array.
 Console.WriteLine(rainbowColours[5].ToUpper());
+
 // Comment out line 6 for testing.
 Console.WriteLine(exampleStringArray[0] == null);
+
+string pigLatinMessage = ""; // ROYGBIV
+for (int i = 0; i < rainbowColours.Length; i++)
+{
+    pigLatinMessage += rainbowColours[i].Substring(1, rainbowColours[i].Length - 1);
+    pigLatinMessage += rainbowColours[i][0];
+    pigLatinMessage += "ay ";
+}
+Console.WriteLine(pigLatinMessage);
