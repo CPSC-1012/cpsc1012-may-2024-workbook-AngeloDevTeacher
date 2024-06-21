752bb5a1fc001336b1f902b5872fe5fc7e01f087
AngeloDevAB <angeloe@nait.ca>
2024-06-20 19:31:06 -0600

    null checking

--- workbook/ArrayPractice/Program.cs
+++ workbook/ArrayPractice/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// This is an exmaple of a list that was declared and initalizxed with values.
+// This is an exmaple of a list that was declared and initalized with values.
 string[] rainbowColours = { "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" };
+string[] exampleStringArray = new string[10];
+exampleStringArray[0] = "Hello";
 
+// Get green from the array.
+Console.WriteLine(rainbowColours[3]);
+
+Console.WriteLine(rainbowColours[5].ToUpper());
+// Comment out line 6 for testing.
+Console.WriteLine(exampleStringArray[0] == null);
