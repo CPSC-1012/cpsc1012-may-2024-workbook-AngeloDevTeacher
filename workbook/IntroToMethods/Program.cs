2100b9db4867a93f7a5e479cfede0c3f1b6de3fc
AngeloDevAB <angeloe@nait.ca>
2024-06-25 19:20:42 -0600

    Created IntroToFunctions

---
+++ workbook/IntroToMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Functions and Methods are ways to reuse our code.
+// Functions exist OUTSIDE and object, methods exist WITHIN an object.
+
+// We've been using methods throughout our course.
+// Console.<thing>() or <thing>.Parse() are examples
+
+// A function is NOT defined within an object and is not used as a method.
+
+int myNumber = 3;
+int yourNumber = 10;
+
+
+
+void GreaterThanFive(int numberToCompare)
+{
+    if (numberToCompare > 5)
+    {
+        Console.WriteLine("The number is greater than 5");
+    }
+}
+
+GreaterThanFive(myNumber);
+GreaterThanFive(yourNumber);
+GreaterThanFive(15);
+
