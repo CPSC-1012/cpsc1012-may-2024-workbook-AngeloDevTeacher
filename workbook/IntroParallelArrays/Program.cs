34f9b8afd31c75bedbcb3ff55f765857e18a9c8e
AngeloDevAB <angeloe@nait.ca>
2024-07-02 21:19:51 -0600

    Arrays examples

---
+++ workbook/IntroParallelArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Corresponding / Parallel Arrays hold data related to each other.
+
+// Max 5 items in these arrays.
+const int ARRAY_SIZE = 5;
+
+// These arrays will have related data and are the same size.
+int[] ids = new int[ARRAY_SIZE];
+string[] names = new string[ARRAY_SIZE];
+float[] grades = new float[ARRAY_SIZE];
+
+static void WriteStudentID(int index, int id, ref int[] array)
+{
+    array[index] = id;
+}
+static void WriteStudentName(int index, string name, ref string[] array)
+{
+    array[index] = name;
+}
+static void WriteStudentGrade(int index, float grade, ref float[] array)
+{
+    array[index] = grade;
+}
+
+
+WriteStudentID(0, 2004, ref ids);
+WriteStudentName(0, "Angelo", ref names);
+WriteStudentGrade(0, 49.9f, ref grades);
+Console.WriteLine($"{ids[0]}, {names[0]}, {grades[0]}");
+
+for (int i = 0; i < ARRAY_SIZE; i++)
+{
+    Console.Write("Enter an ID: ");
+    int idExample = int.Parse(Console.ReadLine());
+    WriteStudentID(i, idExample, ref ids);
+
+    Console.Write($"Enter an name for {ids[i]}: ");
+    string nameExample = Console.ReadLine();
+    WriteStudentName(i, nameExample, ref names);
+}
+for (int i = 0; i < ARRAY_SIZE; i++)
+{
+    Console.WriteLine($"{ids[i]}, {names[i]}, {grades[i]}");
+}
---
+++ workbook/SwitchAndArray/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+const int MAX_SIZE = 15;
+
+int[] ids = new int[MAX_SIZE];
+int[] phoneNumbers = new int[MAX_SIZE];
+string[] names = new string[MAX_SIZE];
+
+
+string userInput = "";
+bool isRunning = true;
+
+Console.WriteLine("Choose an Option: ");
+Console.WriteLine("[I] - ID");
+Console.WriteLine("[P] - Phone Numbers");
+Console.WriteLine("[N] - First Name");
+
+
+do
+{
+	try
+	{
+		Console.Write("Please enter a choice: ");
+		userInput = Console.ReadLine();
+		userInput = userInput.ToUpper();
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine("Please choose \"I\", \"P\", \"N\" ");
+	}
+	if (userInput != "I" && userInput != "P" && userInput != "N")
+	{
+		Console.WriteLine("Please choose \"I\", \"P\", \"N\" ");
+	}
+} while (userInput != "I" && userInput != "P" && userInput != "N");
+
+
+switch (userInput)
+{
+	case "I":
+        Console.Write("Please enter an ID: ");
+		ids[0] = int.Parse(Console.ReadLine());
+        break;
+	case "P":
+        Console.Write("Please enter an phone number: ");
+        phoneNumbers[0] = int.Parse(Console.ReadLine());
+        break;
+	case "N":
+        Console.Write("Please enter an name: ");
+        names[0] = Console.ReadLine();
+        break;
+	default:
+		break;
+}
+Console.WriteLine($"{ids[0]},{phoneNumbers[0]},{names[0]}");
