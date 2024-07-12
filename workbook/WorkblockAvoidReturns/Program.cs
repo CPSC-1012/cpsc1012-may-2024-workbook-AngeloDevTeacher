73e6f6b59931063c1442925e9fc92c4af4d5fa7e
angebl0w <encarnacion.angelo@gmail.com>
2024-07-11 20:02:00 -0600

    Made the valid check

--- workbook/WorkblockAvoidReturns/Program.cs
+++ workbook/WorkblockAvoidReturns/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+const int MAX_PEOPLE = 10;
+
+
+// This is now a descriptive loop - I can read this and figure out what's going on!
+//for (int currentPerson = 0; currentPerson < MAX_PEOPLE; currentPerson++)
+//{
+//    // BELOW IS BAD PRACTICE!!
+//    //if (currentPerson == 3)
+//    //{
+//    //    Console.WriteLine("It's the third person, let's break the loop!");
+//    //    break;
+//    //}
+//    Console.WriteLine(currentPerson.ToString());
+//}
+
+// A better way to prevent code from running!
+
+//Swap the isValid method to test!
+bool isValid = false;
+string userInput = "";
+int numberToCheckNext = 0;
+
+// This prevents the question if the answer lead to invalidate the program!
+if (isValid)
+{
+    Console.Write("Please enter a number:\t");
+    userInput = Console.ReadLine();
+
+
+    if (isValid && int.TryParse(userInput, out numberToCheckNext))
+    {
+        Console.WriteLine(numberToCheckNext);
+    }
+    else
+    {
+
+    }
+}
+
