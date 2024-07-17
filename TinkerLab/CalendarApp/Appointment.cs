ff915d71d44141c1ce3af032f8ca405e826dfe20
angebl0w <encarnacion.angelo@gmail.com>
2024-07-16 18:32:57 -0600

    Setup for later demos

---
+++ TinkerLab/CalendarApp/Appointment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApp
+{
+    /// <summary>
+    /// An appointment - described by a name and date.
+    /// </summary>
+    internal class Appointment
+    {
+        private string _appointmentName;
+        private DateTime _appointmentDate;
+
+        public string AppointmentName
+        {
+            get
+            {
+                return _appointmentName;
+            }
+            set
+            {
+                if (value.Trim() != string.Empty)
+                {
+                    _appointmentName = value;
+                }
+                else
+                {
+                    throw new Exception("The appointment name cannot be empty.");
+                }
+            }
+        }
+
+        public DateTime AppointmentDate
+        {
+            get
+            {
+                return _appointmentDate;
+            }
+            set
+            {
+                if(value.ToString() == string.Empty || !DateTime.TryParse(value.ToString(), out _appointmentDate))
+                {
+                    throw new Exception("Date could not be set.");
+                }
+                
+            }
+        }
+        /// <summary>
+        /// Create a new <c>appointment</c> using <cref>appointmentName</cref> and <cref>appointmentDate</cref>.
+        /// </summary>
+        /// <param name="name">The name of the appointment.</param>
+        /// <param name="date">The date of the appointment.</param>
+        public Appointment(string name, DateTime date) 
+        {
+            _appointmentName = name;
+            _appointmentDate = date;
+        }
+        public override string ToString()
+        {
+            return $"{_appointmentName}, {_appointmentDate.ToShortDateString()}";
+        }
+    }
+
+
+}
---
+++ TinkerLab/CalendarApp/CalendarProgram.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalendarApp;
+
+/// <summary>
+/// This project will allow you to create and read appointments and display them on a calendar.
+/// The appointments should save the appointment name and the appointment date.
+/// Author: Angelo Encarnacion
+/// Last Modified: July 16th 2024
+/// </summary>
+/// 
+
+// General Plan:
+// Display program intro
+// Display Menu
+// Prompt for Option:
+//  1 - Create appointment
+//  2 - Display appointments (by month)
+//  3 - Display appointments (by name?)
+//  0 - Quit the program
+
+//
+//
+
+void DisplayProgramIntro()
+{
+
+}
+
+void DisplayProgramMenu()
+{
+
+}
+
+Appointment thisAppointment = new Appointment("Hello", DateTime.Parse("July 17, 2024"));
+
+
+
+Console.WriteLine(thisAppointment.ToString());
