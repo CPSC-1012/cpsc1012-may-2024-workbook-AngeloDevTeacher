2ec88d6d52bb2ababf9b5b348f2b05662c4049c4
angebl0w <encarnacion.angelo@gmail.com>
2024-08-01 19:54:50 -0600

    Made birds and added to list

--- workbook/PetExamples/PetProgram.cs
+++ workbook/PetExamples/PetProgram.cs
@@ -6,33 +6,13 @@ using PetExamples;
 
 const int MaxPetCount = 3;
 
-string catNameOne = "Sully";
-string catNameTwo = "Piper";
-string catNameThree = "Mike";
 
-string catBreedOne = "Domestic Short Hair";
-string catBreedTwo = "Tripoint Ragdoll";
-string catBreedThree = "Domestic Short Hair";
+Pet[] pets = new Pet[MaxPetCount];
 
-int catAgeOne = 3;
-int catAgeTwo = 2;
-int catAgeThree = 1;
 
-string catColorDescriptionOne = "Tuxedo pattern";
-string catColorDescriptionTwo = "Tri Color";
-string catColorDescriptionThree = "Tuxedo pattern";
+pets[0] = new Cat("Sully", "Domestic Short Hair", 3, "Tuxedo pattern");
+pets[1] = new Cat("Mike", "Domestic Short Hair", 1, "Tuxedo pattern");
+pets[2] = new Bird("Charlie", "Parrot", 973, "Green");
 
-//string[] catNames = new string[MaxCatCount];
-
-string[] catNames = new string[] { "Sully", "Piper", "Mike" };
-string[] catBreeds = new string[] { "Domestic Short Hair", "Tripoint Ragdoll", "Domestic Short Hair" };
-int[] catAges = new int[] { 3, 2, 1 };
-string[] catColorDescriptions = new string[] { "Tuxedo pattern", "Tri Color", "Tuxedo pattern" };
-
-Cat[] cats = new Cat[MaxPetCount];
-
-cats[0] = new Cat();
-cats[1] = new Cat("Sully", "Domestic Short Hair", 3, "Tuxedo pattern");
-
-Console.WriteLine(cats[1].Name);
+Console.WriteLine(pets[1].Name);
 
