abd981e18535f95c386bcb8538b8e40d570f51bd
angebl0w <encarnacion.angelo@gmail.com>
2024-08-01 21:50:19 -0600

    OOP 2 done, generally.

---
+++ workbook/OOPProblemsTwo/MainProgram.cs
@@ -0,0 +1,100 @@
+﻿using OOPProblemsTwo;
+
+/// <summary>
+/// This program allows the user to display the results of up to 25 quizzes and show them in a table.
+/// Author: Angelo Encarnacion
+/// Date Modified: August 1 2024
+/// </summary>
+
+/// <plan>
+/// Setup variables.
+/// Allow for entry.
+/// Display quizzes entered.
+/// Display average.
+/// </plan>
+
+
+// Variables
+const int MaxQuizAmount = 25;
+int quizCount = 0;
+Quiz[] quizzes = new Quiz[MaxQuizAmount];
+bool isCompleted = false;
+
+// User Variables
+string userInput = string.Empty;
+
+// Main Program
+do
+{
+    // Inputting the quizzes.
+    int thisQuizTotal = PromptInteger("Please enter the quiz's total:");
+    int thisQuizWeight = PromptInteger("Please enter the quiz's weight");
+    quizzes[quizCount] = new Quiz(thisQuizTotal, thisQuizWeight);
+
+    // Set the student name
+    userInput = Prompt("Which student wrote the quiz?");
+    quizzes[quizCount].SetStudentName(userInput);
+
+    // Set the mark
+    int mark = PromptInteger("What did the student earn?");
+    quizzes[quizCount].SetMark(mark);
+
+    quizCount++;
+
+    // Asking if we want more quizzes.
+    userInput = Prompt("Do you want to enter another quiz? [Y/N]");
+    if(userInput.ToUpper() == "N")
+    {
+        isCompleted = true;
+    }
+    if (quizCount == MaxQuizAmount)
+    {
+        Console.WriteLine("Max amount of quizzes reached.");
+        isCompleted = true;
+    }
+} while (isCompleted == false);
+
+Console.WriteLine($"\n{quizCount} quizzes entered.");
+
+// Do the table!
+Console.WriteLine($"{"Student Name:",-16}\t\t{"Total:",6}\t\t{"Mark:",6}\t\t{"Percent",7}");
+for (int currentQuizNumber = 0; currentQuizNumber < quizCount; currentQuizNumber++)
+{
+    Console.WriteLine($"{quizzes[currentQuizNumber].GetStudentName(),-16}\t\t{quizzes[currentQuizNumber].GetTotal(),6}\t\t{quizzes[currentQuizNumber].GetMark(),6}\t\t{quizzes[currentQuizNumber].GetPercentage(),7}");
+}
+
+
+// Do the average, I guess.
+
+
+
+// Functions
+
+static string Prompt(string messagePrompt)
+{
+    Console.Write(messagePrompt+"\t");
+    return Console.ReadLine();
+}
+
+static int PromptInteger(string messagePrompt)
+{
+    int Output = -1;
+    do
+    {
+        try
+        {
+            Console.Write(messagePrompt + "\t");
+            Output = int.Parse(Console.ReadLine());
+            if (Output <= 0)
+            {
+                Console.WriteLine("Sorry, please enter a positive, whole number.");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Please enter a number.");
+            Output = -1;
+        }
+    } while (Output <= 0);
+    return Output;
+}
---
+++ workbook/OOPProblemsTwo/Quiz.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblemsTwo
+{
+    internal class Quiz
+    {
+        private string _studentName;
+        private int _total;
+        private int _mark;
+        private int _weight;
+
+        // Because it MUST be provided when the quiz is created, I care about the GREEDY constructor.
+
+        // Constructor
+        /// <summary>
+        /// Creates the quiz - requires the total and weight.
+        /// </summary>
+        /// <param name="total"></param>
+        /// <param name="weight"></param>
+        public Quiz(int total, int weight)
+        {
+            _total = total;
+            _weight = weight;
+        }
+
+
+        /// <summary>
+        /// Sets the mark of the quiz.
+        /// </summary>
+        /// <param name="mark"></param>
+        public void SetMark(int mark)
+        {
+            _mark = mark;
+        }
+
+        /// <summary>
+        /// Gets the mark.
+        /// </summary>
+        /// <returns>The mark as an integer.</returns>
+        public int GetMark()
+        {
+            return _mark;
+        }
+
+        /// <summary>
+        /// Set the student name for the quiz.
+        /// </summary>
+        /// <param name="name"></param>
+        public void SetStudentName(string name)
+        {
+            _studentName = name;
+        }
+
+        /// <summary>
+        /// Gets the student's name.
+        /// </summary>
+        /// <returns>The name as a string.</returns>
+        public string GetStudentName()
+        {
+            return _studentName;
+        }
+
+        /// <summary>
+        /// Get the quiz total.
+        /// </summary>
+        /// <returns>The total as an int.</returns>
+        public int GetTotal()
+        {
+            return _total;
+        }
+
+        /// <summary>
+        /// Get the quiz weight.
+        /// </summary>
+        /// <returns>The weight as an int.</returns>
+        public int GetWeight()
+        {
+            return _weight;
+        }
+
+        /// <summary>
+        /// Gets the percentage of mark earned from the quiz.
+        /// </summary>
+        /// <returns>An int representing the percent.</returns>
+        public int GetPercentage()
+        {
+            // I'm aware this is interger rounding.
+            return (_mark / _total) * 100;
+        }
+
+    }
+}
