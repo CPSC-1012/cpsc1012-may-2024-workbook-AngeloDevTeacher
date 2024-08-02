e172899e311d4ffd2f8168a050cbdd79f9750657
angebl0w <encarnacion.angelo@gmail.com>
2024-08-01 19:54:50 -0600

    Made birds and added to array of pets

--- workbook/PetExamples/Bird.cs
+++ workbook/PetExamples/Bird.cs
@@ -6,23 +6,9 @@ using System.Threading.Tasks;
 
 namespace PetExamples
 {
-    internal class Pet
+    internal class Bird : Pet
     {
-
-        protected string _petName;
-        protected string _petBreed;
-        protected int _petAge;
-        protected string _petDescription;
-
-        internal string Name
-        {
-            get
-            {
-                return _petName;
-            }
-        }
-
-        public Pet()
+        public Bird()
         {
             _petName = string.Empty;
             _petBreed = string.Empty;
@@ -30,7 +16,7 @@ namespace PetExamples
             _petDescription = string.Empty;
         }
 
-        public Pet(string name, string breed, int age, string description)
+        public Bird(string name, string breed, int age, string description)
         {
             _petName = name;
             _petBreed = breed;
