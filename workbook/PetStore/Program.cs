306ed42ea98cf2e9c2228c2cb7dd93a489d01bb4
angebl0w <encarnacion.angelo@gmail.com>
2024-08-13 19:26:08 -0600

    Started the pet store example

---
+++ workbook/PetStore/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+// Variables
+using PetStore;
+
+/// <summary>
+/// Online Pet Store Program - Allows people to make orders for pickup
+/// Author: Angelo Encarnacion
+/// Modified Date: 08-13-2024
+/// </summary>
+/// 
+string example = "";
+StoreItem item = new StoreItem();
+// Main Program
+item.ProductID = Prompt("Please enter in a product ID:\t");
+Console.WriteLine(item.ProductID);
+
+
+#region Static Methods
+
+
+/// <summary>
+/// Prompts the user to input based off of the messagePrompt.
+/// </summary>
+static string Prompt(string messagePrompt)
+{
+    Console.Write(messagePrompt);
+    return Console.ReadLine();
+}
+
+static double PromptDouble(string messagePrompt)
+{
+    double Output = 0;
+    bool ValidInput = false;
+    do
+    {
+        try
+        {
+            Console.Write(messagePrompt);
+            Output = double.Parse(Console.ReadLine());
+            if (Output < 0)
+            {
+                Console.WriteLine("Please enter a positive number");
+                ValidInput = false;
+            }
+            else
+            {
+                ValidInput = true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    } while (!ValidInput);
+    return Output;
+}
+
+
+#endregion
---
+++ workbook/PetStore/StoreItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    internal class StoreItem
+    {
+        // Fields - Fields are what the class IS, and describe state.
+        private string _productID;
+        private string _productDescription;
+        private double _price;
+
+        // Properties - Getters and Setters.
+        public string ProductID
+        {
+            get
+            {
+                return _productID;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Product ID cannot be set to null, empty, or whitespace.");
+                }
+                _productID = value.Trim();
+            }
+        }
+    }
+}
