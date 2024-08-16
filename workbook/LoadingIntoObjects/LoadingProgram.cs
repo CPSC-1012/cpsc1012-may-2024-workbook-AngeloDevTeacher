99e017c5d84e91934b14bab93a6daa673e291417
angebl0w <encarnacion.angelo@gmail.com>
2024-08-15 20:33:08 -0600

    checkpoint

---
+++ workbook/LoadingIntoObjects/LoadingProgram.cs
@@ -0,0 +1,32 @@
+﻿// LOAD FROM A CSV INTO OBJECTS
+
+// We've been able to load a csv into parallel arrays.
+// Doing it into objects is easy and is clearer in my opinion.
+
+using LoadingIntoObjects;
+
+StreamReader reader = null;
+string filePath = "../../students.csv";
+
+List<Student> students = new List<Student>();
+
+
+if (File.Exists(filePath))
+{
+    reader = new StreamReader(filePath);
+    while(!reader.EndOfStream && reader != null)
+    {
+        string line = reader.ReadLine();
+        string[] items = line.Split(",");
+
+        // This is where things change!
+        int thisID = int.Parse(items[0]);
+        string thisFirstName = items[1];
+        string thisLastName = items[2];
+        double thisGrade = double.Parse(items[3]);
+
+        Student thisStudent = new Student(thisID,thisFirstName,thisLastName,thisGrade);
+
+        students.Add(thisStudent);
+    }
+}
---
+++ workbook/LoadingIntoObjects/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadingIntoObjects
+{
+    internal class Student
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private double _grade;
+
+        public Student(int id, string firstName, string lastName, double grade)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _grade = grade;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{_firstName} {_lastName}";
+            }
+        }
+    }
+}
